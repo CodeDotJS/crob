476717f6d7ff53570de8368632d4aff7cee51519
CodeDotJS <rushforlinux@gmail.com>
2016-06-19 03:48:07 +0530

    clean :lipsitck:

---
+++ cli.js
@@ -0,0 +1,52 @@
+#!/usr/bin/env node
+
+'use strict';
+
+const colors = require('colors/safe');
+const execute = 'chromium-browser ';
+
+const argument = require('path').resolve(process.cwd(), process.argv[2] || '');
+const arg = process.argv[2];
+
+const dotTemplate = colors.cyan.bold('⚬');
+const arrowTemplate = colors.green.bold('➠');
+const siteTemp = colors.green('google.com facebook.com twitter.com');
+
+if (arg === '--help' || arg === '-h') {
+	console.log(colors.cyan(
+`
+ Usage:
+    ${dotTemplate} crob                 ${arrowTemplate}  open chromium browser
+    ${dotTemplate} crob /path/to/file   ${arrowTemplate}  open file under chromium
+    ${dotTemplate} crob -s sitename     ${arrowTemplate}  open any website
+
+ Open Multiple Sites:
+    ${dotTemplate} crob -s ${siteTemp}
+`
+  ));
+	process.exit(1);
+}
+
+if (arg === '-s') {
+	const site1 = process.argv[3];
+	const site2 = process.argv[4];
+	const site3 = process.argv[5];
+	if (site1 && site2 && site3 !== undefined) {
+		require('child_process').exec(`${execute} ${site1} ${site2} ${site3}`);
+	}
+
+	if (site1 && site2 !== undefined && site3 === undefined) {
+		require('child_process').exec(`${execute} ${site1} ${site2}`);
+	}
+
+	if (site1) {
+		require('child_process').exec(`${execute} ${site1}`);
+	}
+	process.exit(1);
+}
+
+if (process.argv[2]) {
+	require('child_process').exec(`${execute} ${argument}`);
+} else {
+	require('child_process').exec('chromium-browser');
+}
