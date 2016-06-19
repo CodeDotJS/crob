a0b76d087f454ceb5c511463ecb96d0182c003e6
CodeDotJS <rushforlinux@gmail.com>
2016-06-19 21:34:20 +0530

    check to version

--- test.js
+++ test.js
@@ -3,11 +3,11 @@ import childProcess from 'child_process';
 import test from 'ava';
 
 test.cb(t => {
-	childProcess.execFile('./cli.js', ['-s google.com'], {
+	childProcess.execFile('./cli.js', ['--version'], {
 		cwd: __dirname
 	}, (err, stdout) => {
 		t.ifError(err);
-		t.true(stdout <= 0);
+		t.true(stdout === '1.0.0');
 		t.end();
 	});
 });
