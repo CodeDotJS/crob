455e844e7368f0e32d40ca449c8e1a29db9d031b
CodeDotJS <rushforlinux@gmail.com>
2016-06-19 20:49:19 +0530

    exit before displaying message, NL[before]req

--- cli.js
+++ cli.js
@@ -50,8 +50,10 @@ if (arg === '-s') {
 
 if (process.argv[2]) {
 	require('child_process').exec(`${execute} ${argument}`);
+
 	process.exit(1);
 } else {
 	require('child_process').exec('chromium-browser');
+
 	process.exit(1);
 }
